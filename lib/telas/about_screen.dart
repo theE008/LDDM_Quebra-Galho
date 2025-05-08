7b731e92793fa40d624c0550195ece40fbfe9821
Thiago Pereira de Oliveira <1494349@sga.pucminas.br>
2025-05-08 11:32:19 -0300

    Modo claro (não funcionando no about)

--- lib/telas/about_screen.dart
+++ lib/telas/about_screen.dart
@@ -6,7 +6,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF121212),
+      backgroundColor: const Color(0xFFFF0000),
       appBar: AppBar(
         backgroundColor: const Color(0xFF121212),
         elevation: 0,
